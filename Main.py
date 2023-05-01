c181ae6f2614cb474dd61451a26545891cc290db
Aung0071 <121412299+Aung0071@users.noreply.github.com>
2023-05-01 18:40:54 -0400

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 name = input('Hello welcome to Cha Ching bank, what is your name?: ')
 Money = 0
 subtract = 0
@@ -39,51 +38,10 @@ def Withdraw():
     global subtract
     Sec = input(f'What is the password to {Username}?: ')
     if Sec == Password:
-=======
-name = input('Hello welcome to Cha Ching bank, what is your name? ')
-action = input(f'welcome, {name} what would you like to do? ')
-
-
-def Makeaccount():
-    global Username
-    global Password
-    while True:
-        Username = input('Please input a username: ')
-        if len(Username) >= 5:
-            break
-        else:
-            print('username to short please input username with 5 or more characters')
-    while True:
-        Password = input('Please input a password: ')
-        if len(Password) >= 5:
-            break
-        else:
-            print('username to short please insert password with 5 or more characters')
-    print(f'Welcome {Username} to Cha Ching banking!')
-
-def Deposit():
-    global Money
-    while True:
-        try:
-            Money = int(input(f'How much would you like to deposit into {Username}?: '))
-            break
-        except ValueError:
-            print('please input a valid number!')
-
-    
-    print(f'{Money} has been deposited into {Username}')
-
-def Withdraw():
-    global Newcash
-    global subtract
-    Sec = input(f'what is the password to {Username}?: ')
-    if Sec == (Password):
->>>>>>> 51dfcd15c7ca82b33e605c46d7fe14bd871a9409
         while True:
             try:
                 takeout = int(input(f'How much would you like to withdraw from {Username}?: '))
                 if takeout > Money:
-<<<<<<< HEAD
                     print("You're withdrawing more money than you have!")
                 else:
                     subtract = Money - takeout
@@ -172,23 +130,3 @@ while True:
     elif action == '6':
       break
 
-
-=======
-                    print('looks like your gonna be in dept!')
-                else:
-                    subtract = Money - takeout
-                    break
-            except ValueError:
-                print('please input a valid number!')
-                
-        
-        Newcash = print(f'you now have {subtract} in {Username}')
-    else:
-        print('FRAUD DETECTED GET OUT OF THIS ACCOUNT! Now everything will be reset this is how we ensure our bank is totally safe -_-')
-
-                
-
-Makeaccount()
-Deposit()
-Withdraw()
->>>>>>> 51dfcd15c7ca82b33e605c46d7fe14bd871a9409
